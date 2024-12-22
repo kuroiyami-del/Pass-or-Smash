7d726c88ad3fbba44d75aa67266aeddc2545f3ae
Kuro <kuroiyami720@gmail.com>
2024-12-22 13:07:42 -0500

    Actualizacion de las funciones de pokemones para traer la imagenes junto a sus nombres

--- pokeAPI.py
+++ pokeAPI.py
@@ -2,20 +2,38 @@ import requests
 import random
 
 
-def pokemonesRandom():
+def cargarPokemonesRandom(cantidad=10):
+    lista_pokemones = []
+
     URL = "https://pokeapi.co/api/v2/pokemon/"
-    respuesta = requests.get(URL + str(random.randint(1, 101)))
-    datos = respuesta.json()
 
-    for nombre in datos["forms"]:
-        return nombre["name"]
+    for i in range(cantidad):
+        id_random = random.randint(1, 151)
 
-
-def pokemonesOrden():
-    URL = "https://pokeapi.co/api/v2/pokemon/"
-    for i in range(1, 101):
-        respuesta = requests.get(URL + str(i))
+        respuesta = requests.get(URL + str(id_random))
         datos = respuesta.json()
 
-        for nombre in datos["forms"]:
-            print(nombre["name"])
+        nombre = datos["forms"][0]["name"]
+        imagen_url = datos["sprites"]["other"]["official-artwork"]["front_default"]
+
+        lista_pokemones.append({"nombre": nombre, "imagen": imagen_url})
+
+    return lista_pokemones
+
+
+def cargarPokemonesOrden(cantidad=10):
+    lista_pokemones = []
+
+    URL = "https://pokeapi.co/api/v2/pokemon/"
+
+    for i in range(cantidad):
+        id_ramdon = random.randint(1, 151)
+        respuesta = requests.get(URL + str(id_ramdon))
+        datos = respuesta.json()
+
+        nombres = datos["forms"]["name"]
+        imagen_url = datos["sprites"]["other"]["official-artwork"]["front_default"]
+
+        lista_pokemones.append({"nombre": nombres, "imagen": imagen_url})
+
+    return lista_pokemones
