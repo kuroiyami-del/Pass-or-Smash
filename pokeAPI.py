2acd33a34d2a4610202e2469fa3f3d2b08b97fca
Kuro <kuroiyami720@gmail.com>
2024-12-22 14:02:37 -0500

    Se agrego la funcion para porder mostrar los pokemones

--- pokeAPI.py
+++ pokeAPI.py
@@ -1,7 +1,11 @@
+import tkinter as tk
+from PIL import Image, ImageTk
 import requests
+from io import BytesIO
 import random
 
 
+# Funcion para tener pokemones random
 def cargarPokemonesRandom(cantidad=10):
     lista_pokemones = []
 
@@ -21,6 +25,25 @@ def cargarPokemonesRandom(cantidad=10):
     return lista_pokemones
 
 
+def mostrarPokemon(index, pokemones, label_imagen, label_nombre):
+    """Muestra el Pokémon actual en la interfaz."""
+    # Obtiene el Pokémon actual de la lista usando el índice
+    pokemon = pokemones[index]
+
+    # Descarga la imagen del Pokémon desde la URL
+    response = requests.get(pokemon["imagen"])
+    imagen = Image.open(BytesIO(response.content))
+    imagen = imagen.resize((200, 200))  # Ajusta el tamaño de la imagen
+    pokemon_imagen = ImageTk.PhotoImage(imagen)
+
+    # Actualiza el Label con la nueva imagen
+    label_imagen.config(image=pokemon_imagen)
+    label_imagen.image = pokemon_imagen  # Importante para evitar que la imagen sea recolectada por el garbage collector
+
+    # Actualiza el Label con el nombre del Pokémon
+    label_nombre.config(text=pokemon["nombre"].capitalize())
+
+
 def cargarPokemonesOrden(cantidad=10):
     lista_pokemones = []
 
@@ -31,7 +54,7 @@ def cargarPokemonesOrden(cantidad=10):
         respuesta = requests.get(URL + str(id_ramdon))
         datos = respuesta.json()
 
-        nombres = datos["forms"]["name"]
+        nombres = datos["forms"][0]["name"]
         imagen_url = datos["sprites"]["other"]["official-artwork"]["front_default"]
 
         lista_pokemones.append({"nombre": nombres, "imagen": imagen_url})
